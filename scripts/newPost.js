b7f861ba02db4b429953e15b2619cf6e016088a3
Zoya Wajzer <zoyamber@gmail.com>
2024-03-27 08:49:02 -0700

    initial

---
+++ scripts/newPost.js
@@ -0,0 +1,20 @@
+/** COMPLETE THIS PART */
+async function addPost(post) {
+    /**
+     * 1. Get the list posts from local storage
+     *      if local storage is empty, initialize the list of posts to an empty array
+     * 2. Add the new post at the front of list
+     * 3. Save the list posts back to local storage
+     */
+
+    // Your code here
+}
+
+
+document.querySelector('#new-post').addEventListener('submit', (e) => {
+    e.preventDefault();
+    const title = e.target.title.value;
+    const body = e.target.body.value;
+    addPost({title, body});
+    e.target.reset();
+});
